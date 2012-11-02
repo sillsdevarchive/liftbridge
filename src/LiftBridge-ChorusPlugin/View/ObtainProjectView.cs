52e6b9cd6a0c9dd50dab14cbcae42fdfc4d246ba
Randy Regnier <rbregnier@gmail.com>
2012-11-02 15:54:30 -0600

    Remove old start/use code and replace with Flex Brdige-like UI.

---
+++ src/LiftBridge-ChorusPlugin/View/ObtainProjectView.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace SIL.LiftBridge.View
+{
+	public partial class ObtainProjectView : Form
+	{
+		public ObtainProjectView()
+		{
+			InitializeComponent();
+		}
+	}
+}
